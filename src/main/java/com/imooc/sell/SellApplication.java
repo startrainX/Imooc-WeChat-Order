d02fc59fd0c8f68aba2cc1b4bcb15e2b839850cd
crazy <1451023577@qq.com>
2019-04-30 17:18:07 +0800

    log配置

--- src/main/java/com/imooc/sell/SellApplication.java
+++ src/main/java/com/imooc/sell/SellApplication.java
@@ -3,6 +3,10 @@ package com.imooc.sell;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author zsp
+ * @Data: 2019/4/30
+ */
 @SpringBootApplication
 public class SellApplication {
 
