fb69f3c9ba31b54453c412477d91ca3a9e7cf93c
crazy <1451023577@qq.com>
2019-05-17 16:00:25 +0800

    结束了，好好捋捋代码

--- src/main/java/com/imooc/sell/vo/ResultVo.java
+++ src/main/java/com/imooc/sell/vo/ResultVo.java
@@ -2,14 +2,17 @@ package com.imooc.sell.vo;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * http请求返回的最外层对象 ViewObject
  * @author ：zsp
  * @date ：Created in 2019/5/5 16:50
  */
 @Data
-public class ResultVo<T> {
+public class ResultVo<T> implements Serializable{
 
+    private static final long serialVersionUID = -5638414267303610471L;
     /** 错误码 */
     private Integer code;
 
